5c1d9cd88c6a42140804fdf37a7a0fc9793a0f3e
LeoPlugged <leandro.miguel@outlook.com>
2022-07-11 19:34:06 -0300

    Add updates (API)

---
+++ app/src/main/java/br/com/zup/apprickymorty/ui/view/HomeActivity.kt
@@ -0,0 +1,12 @@
+package br.com.zup.apprickymorty.ui.view
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import br.com.zup.apprickymorty.R
+
+class HomeActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_home)
+    }
+}
